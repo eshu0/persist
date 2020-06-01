f58f12adefa20631fd5369b6d83ea97512161d29
eshu0 <willsawesometoolkit@gmail.com>
2020-06-01 15:51:26 +0100

    Update SQLLiteTableHandler.go

--- pkg/sqllite/SQLLiteTableHandler.go
+++ pkg/sqllite/SQLLiteTableHandler.go
@@ -27,7 +27,7 @@ func (handler *SQLLiteTableHandler) GetPersistantStorage() per.IPersistantStorag
 }
 
 func (handler *SQLLiteTableHandler) SetPersistantStorage(persistant per.IPersistantStorage){
-	res := persistant.(per.IPersistantStorage)
+	res := persistant.(*SQLLiteDatastore)
 	handler.Parent = res
 }
 
