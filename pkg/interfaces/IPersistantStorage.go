f040b6698f200ad6d0c933be532f038550373ac5
eshu0 <willsawesometoolkit@gmail.com>
2020-06-01 15:06:55 +0100

    Update IPersistantStorage.go

--- pkg/interfaces/IPersistantStorage.go
+++ pkg/interfaces/IPersistantStorage.go
@@ -7,9 +7,9 @@ import (
 type IPersistantStorage interface {
 
 	// Get/Set the logging for the interface
-	GetStorageHandler(name string) (IStorageHandler, bool)
-	SetStorageHander(name string, store IStorageHandler)
-	GetAllStorageHandlers() map[string]IStorageHandler
+	GetStorageHandler(name string) (*IStorageHandler, bool)
+	SetStorageHander(name string, store *IStorageHandler)
+	GetAllStorageHandlers() map[string]*IStorageHandler
 	RemoveStorageHandler(name string) bool
 
 	// Get/Set the logging for the interface
