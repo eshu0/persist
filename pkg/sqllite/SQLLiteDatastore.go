9914428f531aa29810f8120e5c1c6f7f97cd3182
eshu0 <willsawesometoolkit@gmail.com>
2020-12-12 20:40:09 +0000

    Update SQLLiteDatastore.go

--- pkg/sqllite/SQLLiteDatastore.go
+++ pkg/sqllite/SQLLiteDatastore.go
@@ -3,10 +3,10 @@ package SQLL
 import (
 	"database/sql"
 
-	_ "github.com/mattn/go-sqlite3"
-	sli "github.com/eshu0/simplelogger/interfaces"
 	per "github.com/eshu0/persist/pkg/interfaces"
-)   
+	sli "github.com/eshu0/simplelogger/pkg/interfaces"
+	_ "github.com/mattn/go-sqlite3"
+)
 
 type SQLLiteDatastore struct {
 	per.IPersistantStorage
@@ -22,7 +22,7 @@ type SQLLiteDatastore struct {
 
 func CreateSQLLiteDatastore(log sli.ISimpleLogger, filename string) *SQLLiteDatastore {
 	sqlds := SQLLiteDatastore{}
-	sqlds.Filename =filename
+	sqlds.Filename = filename
 	sqlds.Log = log
 	StorageHandlers := make(map[string]per.IStorageHandler)
 	sqlds.StorageHandlers = StorageHandlers
@@ -35,54 +35,52 @@ func CreateOpenSQLLiteDatastore(log sli.ISimpleLogger, filename string) *SQLLite
 	return sqlds
 }
 
-
 func (sqlds *SQLLiteDatastore) Open() {
 	// further checks to be added here like checking filepath is correct etc
 	sqlds.database, _ = sql.Open("sqlite3", sqlds.Filename)
 }
 
-// Storage Handlers 
+// Storage Handlers
 // in this implementation that is Tables
 
 func (sqlds *SQLLiteDatastore) GetStorageHandler(name string) (per.IStorageHandler, bool) {
-	res, ok :=  sqlds.StorageHandlers[name]
-	return res,ok
+	res, ok := sqlds.StorageHandlers[name]
+	return res, ok
 }
 
 func (sqlds *SQLLiteDatastore) SetStorageHander(name string, handler per.IStorageHandler) {
 	//(*store).SetPersistantStorage(sqlds)
 	//ss := *store
-		//res := handler.(*SQLLiteTableHandler)
+	//res := handler.(*SQLLiteTableHandler)
 	//res.Parent = sqlds
-	sqlds.Log.LogDebugf("SetStorageHander","Setting %s, %v", name, handler)
+	sqlds.Log.LogDebugf("SetStorageHander", "Setting %s, %v", name, handler)
 	//newhandler := handler.SetPersistantStorage(sqlds)
 	//handlers := sqlds.StorageHandlers
 	//handlers[name] = newhandler
 	//sqlds.StorageHandlers = handlers
 	sqlds.StorageHandlers[name] = handler
-	sqlds.Log.LogDebugf("SetStorageHander","Handlers = %v", name, sqlds.StorageHandlers)
+	sqlds.Log.LogDebugf("SetStorageHander", "Handlers = %v", name, sqlds.StorageHandlers)
 
 }
 
 func (sqlds *SQLLiteDatastore) RemoveStorageHandler(name string) bool {
 	_, ok := sqlds.StorageHandlers[name]
-    if ok {
-        delete(sqlds.StorageHandlers, name)
-    }
+	if ok {
+		delete(sqlds.StorageHandlers, name)
+	}
 	return ok
 }
-
 
 func (sqlds *SQLLiteDatastore) GetAllStorageHandlers() map[string]per.IStorageHandler {
 	return sqlds.StorageHandlers
 }
-	
+
 // Get/Set the logging for the interface
 func (sqlds *SQLLiteDatastore) GetLog() sli.ISimpleLogger {
 	return sqlds.Log
 }
 
-func (sqlds *SQLLiteDatastore) SetLog(logger sli.ISimpleLogger){
+func (sqlds *SQLLiteDatastore) SetLog(logger sli.ISimpleLogger) {
 	sqlds.Log = logger
 }
 
@@ -91,10 +89,10 @@ func (sqlds *SQLLiteDatastore) GetDatabase() *sql.DB {
 }
 
 func (sqlds *SQLLiteDatastore) CreateStructures() per.IQueryResult {
-	
+
 	success := NewEmptySucceedSQLLiteQueryResult()
 	for key, element := range sqlds.StorageHandlers {
-		sqlds.Log.LogDebugf("CreateStructures","Handling %s, %v", key, element)
+		sqlds.Log.LogDebugf("CreateStructures", "Handling %s, %v", key, element)
 		res := element.CreateStructures()
 		if !res.QuerySucceeded() {
 			success = NewEmptyFailedSQLLiteQueryResult()
@@ -108,7 +106,7 @@ func (sqlds *SQLLiteDatastore) Wipe() per.IQueryResult{
 	success := NewEmptySucceedSQLLiteQueryResult()
 	for key, element := range sqlds.StorageHandlers {
 		sqlds.Log.LogDebugf("Wipe","Handling %s, %v", key, element)
-		res :=  element.Wipe() 
+		res :=  element.Wipe()
 		if !res.QuerySucceeded() {
 			success = NewEmptyFailedSQLLiteQueryResult()
 		}
